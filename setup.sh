3e71266bd8d42fcfe2d2f93a577054c96a1e479d
Jacob <jakehilliard01@gmail.com>
2021-01-12 13:19:52 -0500

    fixing up shell script

--- setup.sh
+++ setup.sh
@@ -1,11 +1,16 @@
-#!bin/bash
+#!/bin/bash
 echo 'Beginning setup for discord.pi...'
+
+#Pull dependenices
+echo 'Pulling dependencies...'
+cd "$(dirname "$0")" || exit
+npm install
 
 #Add bot key to .env.example and rename
 echo 'Input your Discord bot token.'
 read -rp 'Token: ' TOKEN
 TOKEN_LENGTH=${#TOKEN}
-DESIRED_TOKEN_LENGTH=79
+DESIRED_TOKEN_LENGTH=59
 
 #Check token length
 if [ ${TOKEN_LENGTH} -ne ${DESIRED_TOKEN_LENGTH} ]
@@ -13,20 +18,21 @@ then
     echo 'Incorrect key length.'
     exit
 fi
-echo 'Adding key to .env'
+echo 'Adding key to .env...'
 sed "s/replace/${TOKEN}/" .env.example > .env
 echo 'Key added.'
 
 #Autostart setup
 echo 'Discord.js can autostart for easier headless use, would you like to enable?'
-read  -e -r -p "Y/n" PROMPT_RESULT
+read  -e -r -p "Y/n: " PROMPT_RESULT
 if [[ ${PROMPT_RESULT} == [Nn]* ]]
 then
     echo 'Autostart not enabled.'
     exit
 fi
+echo 'Adding to rc.local...'
 cd "$(dirname "$0")/src/" || exit
 COMMAND="node ${PWD}/index.js &"
-sed "\$i${COMMAND}"/etc/rc.local
+sed -i "\$i${COMMAND}" /etc/rc.local
 
 echo 'Added to rc.local, restart to test.'
